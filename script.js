ca05351daa5adbda86e75f2e5975ee53ba5a270b
Ovzmwil <leandro.rodrigues.santana@hotmail.com>
2025-05-02 11:53:11 -0300

    K6 Testing example, with report

---
+++ script.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    duration: '5m',
+    vus: 500,
+    http_req_failed: ['rate<0.01']
+};
+
+export default function () {
+    const id = Math.floor(Math.random() * 500);
+    const url = `https://jsonplaceholder.typicode.com/comments/${id}`;
+    const res = http.get(url);
+
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+        'response time < 1s': (r) => r.timings.duration < 1000,
+        'response body is not empty': (r) => r.body.length > 0,
+    });
+
+    sleep(1);
+}
